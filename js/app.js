5ab5c97c4348f11e5ceffe7c9cd5a991435d00c8
hacknt <untadc@gmail.com>
2016-01-14 20:30:13 -0600

    updated entire site.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,49 @@
-$(document).foundation();
+$(document).ready(function () {
+    
+    (function () {
+    var words = [
+        'pursuing a career in UX design &amp; research',
+        'working at UNT ITSS',
+        'planning a hackathon',
+        'enjoying some coffee at West Oak',
+        'craving some tacos right about now'
+        ],
+        i = 0;
+
+    setInterval(function () {
+        $('.changingwords').fadeOut(function () {
+            $(this).html(words[i = (i + 1) % words.length]).fadeIn();
+        });
+        // 3 seconds
+    }, 4000);
+
+})();
+    
+    
+        $('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+
+scrollIntervalID = setInterval(stickIt, 10);
+
+
+function stickIt() {
+
+  var orgElementPos = $('.original').offset();
+  orgElementTop = orgElementPos.top;               
+
+  if ($(window).scrollTop() >= (orgElementTop)) {
+    // scrolled past the original position; now only show the cloned, sticky element.
+
+    // Cloned element should always have same left position and width as original element.     
+    orgElement = $('.original');
+    coordsOrgElement = orgElement.offset();
+    leftOrgElement = coordsOrgElement.left;  
+    widthOrgElement = orgElement.css('width');
+    $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
+    $('.original').css('visibility','hidden');
+  } else {
+    // not scrolled past the menu; only show the original menu.
+    $('.cloned').hide();
+    $('.original').css('visibility','visible');
+  }
+}
+});
